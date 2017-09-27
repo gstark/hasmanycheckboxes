08457c62f3b86f88fe7062b719dc768083ba6306
Gavin Stark <gavin@gstark.com>
2017-09-27 10:37:53 -0400

    Adds ActiveRecord relationships

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+  has_many :memberships, dependent: :destroy
+  has_many :teams, through: :memberships
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,4 @@
 class Team < ApplicationRecord
+  has_many :memberships, dependent: :destroy
+  has_many :players, through: :memberships
 end
